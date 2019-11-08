80e60e8432833a445171ac6928264196c2d7766a
uselessalias <danmason721@gmail.com>
2019-11-08 15:44:03 +0000

    '2'

--- .travis/deploy_docker_image.sh
+++ .travis/deploy_docker_image.sh
@@ -2,5 +2,5 @@
 
 docker login -u $DOCKER_USERNAME -p $DOCKER_PASSWORD
 TAG="latest"
-docker build -t scrapebbc:$TAG .
-docker push scrapebbc:$TAG
+docker build -t $DOCKER_USERNAME/scrapebbc:$TAG .
+docker push $DOCKER_USERNAME/scrapebbc:$TAG
